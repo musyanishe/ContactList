d1c885b631ed4a114098f5c22139cae6622d117f
Evgeniya Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-03-21 22:48:05 +0300

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Евгения Шевцова on 21.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
